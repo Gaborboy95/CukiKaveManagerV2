bed8b835d6df05027f1dc79bfb20624c9281f83e
Peter Gabor <peter.gabor04@gmail.com>
2019-12-16 17:53:13 +0100

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using CukiKaveManagerV2.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CukiKaveManagerV2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            for (int i = 0; i < 10; i++)
+            {
+                ItemHolder.Children.Add(new flippable("Teszt " + i, i * 1000, "Lorem ipsum " + i));
+            }
+        }
+    }
+}
---
+++ UserControls/flippable.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CukiKaveManagerV2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for flippable.xaml
+    /// </summary>
+    public partial class flippable : UserControl
+    {
+
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string Description { get; set; }
+
+        public flippable(string _title, int _price, string _desc)
+        {
+            InitializeComponent();
+
+            Title = _title;
+            Price = _price;
+            Description = _desc;
+        }
+    }
+}
